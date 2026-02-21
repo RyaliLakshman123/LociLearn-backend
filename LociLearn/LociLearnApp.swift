771c0f73fa2283f11b7a6afc765f4a31c534dfc1
ryalilakshman123 <lakshmanryali123@gmail.com>
2026-02-21 20:34:55 +0530

    Initial Commit

---
+++ LociLearn/LociLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LociLearnApp.swift
+//  LociLearn
+//
+//  Created by Sameer Nikhil on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LociLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
